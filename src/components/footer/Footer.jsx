2ceef67e3460056b524dd05f11b9ccb28f013eb4
osunlaja abiodun <osunlajajamiu97@gmail.com>
2023-09-10 23:41:40 +0100

    fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Footer() {
   const year = new Date().getFullYear().toString();
   return (
-    <footer className=' bg-lightPurple mt-28 p-5 text-center'>
+    <footer className='bg-lightPurple mt-28 p-5 text-center'>
       <Typography fontWeight={100} color='#7B4AE2'>
         Copyright © Osunlaja Abiodun · {year}
       </Typography>
